51371ebb74dad75d0906134e056e0b2883f96269
alysonz <alysonz@gmail.com>
2013-09-11 21:26:50 +0000

    heshenames

--- sql.py
+++ sql.py
@@ -1,9 +1,16 @@
 import mysql.connector
 cnx = mysql.connector.connect(user='alyson', password='thetempleofwhollyness', host='localhost', database='heshenames')
 cursor = cnx.cursor()
-query = ("SELECT name, salary FROM doafull")
-cursor.execute(query)
-for (name, salary) in cursor:
-	print name + " " + salary
+d = 1
+while d < 10:
+	query = ("select * from doanames where id=%s;" % (d))
+	cursor.execute(query)
+	for name in cursor:
+		print name
+	d=d+1
+query2 = ("select * from heshe where rank=1")
+cursor.execute(query2)
+for rank in cursor:
+	print rank
 cursor.close()
 cnx.close()
