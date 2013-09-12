fcbf13f6a15dc11d68b7616cec1a99b6e03f13f8
alysonz <alysonz@gmail.com>
2013-09-12 17:05:55 +0000

    heshenames

--- sql.py
+++ sql.py
@@ -3,10 +3,16 @@ cnx = mysql.connector.connect(user='alyson', password='thetempleofwhollyness', h
 cursor = cnx.cursor()
 d = 1
 while d < 10:
-	query = ("select * from doanames where id=%s;" % (d))
+#first take id from doanames 1-10
+	query = ("select id from doanames where id=%s;" %(d))
 	cursor.execute(query)
 	for name in cursor:
-		print name
+		#then use id to find full emp info within doafull
+		query1 = ("select * from doafull where id=%s;" %(name))
+		cursor.execute(query1)
+		for thing in cursor:
+			#print full emp info
+			print thing
 	d=d+1
 query2 = ("select * from heshe where rank=1")
 cursor.execute(query2)
