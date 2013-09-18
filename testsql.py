445865e29c93e6b1dd9ed678ff577adfb84a7ccb
alysonz <alysonz@gmail.com>
2013-09-18 19:06:18 +0000

    heshenames

--- testsql.py
+++ testsql.py
@@ -44,8 +44,16 @@ for line in data:
 		line.extend(['none', 'none', '0.0', '0.0', '0'])
 	if len(line) <> 15:
 		print line
-	query7 = ("insert into final (id, dept, name, title, salary, first, gender, percent, percentile, rank, first2, gender2, percent2, percentile2, rank2) values('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % (line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14]))
+	query7 = ("insert into finall(id, dept, name, title, salary, first, gender, percent, percentile, rank, first2, gender2, percent2, percentile2, rank2) values('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % (line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14]))
 	cursor.execute(query7)
+query8 = ("insert into final(id, dept, name, title, salary, first, gender, percent, percentile, rank, first2, gender2, percent2, percentile2, rank2) values('1','Department of public works', 'Wildfire McGee', 'Badass', '132.40', 'Wildfire', 'F', '0.003', '3.02', '666', 'none', 'none', 'none', 'none', 'none')")
+print query8
+cursor.execute(query8)
+# THIS IS A TEST, FOR THE LOVE OF GOD REMOVE IT BEFORE FINALIZING!!!!!!!!!!!!!!!!!!
+query9 = ("show grants;")
+cursor.execute(query9)
+for thing in cursor:
+	print thing
 cnx.commit()
 print 'Success!'
 cursor.close()
