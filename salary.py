e6cef0d9ed7de7857b5246ebe9f61cd45ea5dc89
alysonz <alysonz@gmail.com>
2013-09-24 20:36:20 +0000

    heshenames

--- salary.py
+++ salary.py
@@ -5,8 +5,8 @@ db = MySQLdb.connect(user='alyson', passwd='thetempleofwhollyness', db='heshenam
 cursor= db.cursor()
 af = []
 am = []
-ab = 1
-no = 1
+ab = []
+no = []
 title = 1
 dept = 1
 #find all distinct departments
@@ -18,32 +18,40 @@ for line in cursor:
 	for a in cursor:
 		title = list(a)
 		#average the salary for all employees of each distinct title in each department where the chance of being female is greater than 7 in 10
-		#HAVE ACCOUNTED FOR NAMES WHERE ONLY MALE DATA APPLIES
-		cursor.execute("select avg(salary) from doagender where dept=%s and title =%s and gender='F' and ((percent*3200000)/((percent2*3000000)+(percent*3200000)))*100 > 70;", (dept[0],title[0]))
+		query1 = "select avg(salary) from doagender where dept=%s and title =%s and gender='F' and ((percent*3200000)/((percent2*3000000)+(percent*3200000)))*100 > 70;", (dept[0],title[0])
+		cursor.execute(query1)
 		for b in cursor:
-			af=list(b)
-			print dept
-			print 'avg fem salary for ' + title[0]
-			print af
+			af=b
+			#af=list(b)
+			#print dept
+			#print 'avg fem salary for ' + title[0]
+			#print af
 		#count how many females have this job title in this department
-
+		cursor.execute("select count(name) from doagender where dept=%s and title =%s and gender='F' and ((percent*3200000)/((percent2*3000000)+(percent*3200000)))*100 > 70;", (dept[0],title[0]))
+		for d in cursor:
+			print d
 		#average the salary for all employees of each distinct title in each department where the chance of being male is greater than 7 in 10
-		#HAVE NOT ACCOUNTED FOR NAMES WHERE ONLY MALE DATA APPLIES
-		cursor.execute("select avg(salary) from doagender where dept=%s and title =%s and ((percent2*3000000)/((percent2*3000000)+(percent*3200000)))*100 > 70 or gender='M';" , (dept[0],title[0]))
+		query2 = "select avg(salary) from doagender where dept=%s and title =%s and (gender='M' or ((percent2*3000000)/((percent2*3000000)+(percent*3200000)))*100 > 70);" , (dept[0],title[0])
+		cursor.execute(query2)
 		for c in cursor:
-			am = list(c)
-			print 'avg male salary for ' + title[0]
-			print am
+			am = c
+			#am = list(c)
+			#print 'avg male salary for ' + title[0]
+			#print am
 		#count how many males have this job title in this department
-
+		cursor.execute("select count(name) from doagender where dept=%s and title =%s and (gender='M' or ((percent2*3000000)/((percent2*3000000)+(percent*3200000)))*100 > 70);" , (dept[0],title[0    ]))
+		for e in cursor:
+			print e
 		#find employees of each distinct title in each department where gender is in question
 		cursor.execute("select * from doagender where dept=%s and title=%s and ((percent*3200000)/((percent2*3000000)+(percent*3200000)))<70 and ((percent*3200000)/((percent2*3000000)+(percent*3200000)))>30;",(dept[0],title[0]))
-		for line in cursor:
-			ab = line
+		for f in cursor:
+			ab = list(f)
 			print 'ambig gender for %s',(title[0])
 			print ab
 		#count how many gender amb employees have this job title in this department
-
+		cursor.execute("select count(name) from doagender where dept=%s and title=%s and ((percent*3200000)/((percent2*3000000)+(percent*3200000)))<70 and ((percent*3200000)/((percent2*3000000)+(percent*3200000)))>30;",(dept[0],title[0]))
+		for g in cursor:
+			print g
 		#find employees of each title in each department where there is no gender data
 		cursor.execute("select * from doagender where dept=%s and title=%s and first='none';",(dept[0],title[0]))
 		for line in cursor:
@@ -51,8 +59,12 @@ for line in cursor:
 			print 'no gender data for emp'
 			print no
 		#count how many emp w/no gender info have this job title in this department
-		
-
+		cursor.execute("select count(name) from doagender where dept=%s and title=%s and first='none';",(dept[0],title[0]))
+		for h in cursor:
+			print h
+		cursor.execute("select %s, %s where %i-%i>.5;",(dept[0],title[0],query1,query2))
+		for i in cursor:
+			print list(i) + " FLAG!!!"
 #create table to load list data information	
 #cursor.execute("create table final (id int(10), dept varchar(40), name varchar(40), title varchar(40), salary decimal (10,5), first varchar(10), gender varchar(2), percent decimal(10,5), percentile decimal(10,5), rank int(10), first2 varchar(10), gender2 varchar(2), percent2 decimal(10,5), percentile2 decimal(10,5), rank2 int(10))")
 #normalize lists within data to contain placeholders for every column in query6 table
